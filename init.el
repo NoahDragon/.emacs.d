0d8f4ac0b50c3e91ac99675ffc2b4036e440a741
Abner Chou <zhcjtht@hotmail.com>
2017-10-16 13:36:29 -0400

    init

---
+++ init.el
@@ -0,0 +1,17 @@
+;; init.el --- Where all them magic begins
+;;
+;; Get rid of the .emacs to be more version control friendly.
+;; All settings should goes here, or sub files.
+;; 
+
+;; set MELPA
+(require 'package)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+  (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/pakcages/"))
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+  (package-initialize)
+  (unless package-archive-contents (package-refresh-contents))
+
+;; Set Evil Mode
+(require 'evil)
+  (evil-mode 1)
