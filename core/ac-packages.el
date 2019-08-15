ed2eb073b7150bd6d810251272e2e0ae1688d830
Abner Chou <hi@abnerchou.me>
2019-08-15 18:21:18 -0400

    add markdown mode

--- core/ac-packages.el
+++ core/ac-packages.el
@@ -431,6 +431,15 @@
     (use-package powershell
       :ensure t)
     )
+
+    (use-package markdown-mode
+      :ensure t
+      :mode ( ("README\\.md\\'" . gfm-mode)
+              ("\\.md\\'" . markdown-mode)
+              ("\\.markdown\\'" . markdown-mode))
+      :init
+      (setq markdown-command "multimarkdown")
+      )
   )
 
 (provide 'ac-packages)
